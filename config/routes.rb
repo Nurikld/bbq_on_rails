82c855d72215a0f99a4751e33fabb7e2cb94fa3e
nuriakgd@gmail.com <nuria>
2023-07-17 14:59:31 +0200

    Create Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   # корень сайта
   root to: "events#index"
+
+  # Не все действия поддерживаются – только show, edit, update
+  resources :users, only: [:show, :edit, :update]
 
   resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
